e259674bef9f86f6e27925ce909b701e0743bd2c
Douglas Uretsky <douglasuretsky@gmail.com>
2020-01-06 09:23:44 -0500

    add up and down arrows to community favorites/users will be able to sort list by ranking

--- client/src/components/BreweriesDisplay.js
+++ client/src/components/BreweriesDisplay.js
@@ -1,19 +1,31 @@
 import '../App.css'
-
+import upArrow from '../images/up-arrow.png';
+import downArrow from '../images/down-arrow.png';
 import React from 'react'
 
 const BreweriesDisplay = (props) => {
+    
+    const voteClick = (e) => {
+        // fetch
+    }
+
     const breweries = props.breweries.map((brewery) => {
-        return <li onClick={props.fetchBreweryById} 
+        return <div key={brewery.id}>
+                <li onClick={props.fetchBreweryById} 
                     key={brewery.id} 
                     value={brewery.openbrewerydb_id ? brewery.openbrewerydb_id : brewery.id}>
                     {brewery.name}
-                </li>
+                    </li>
+                        <img onClick={voteClick} id={brewery.id} className="up-arrow" src={upArrow} height="42" width="42"></img>
+                        <img onClick={voteClick} id={brewery.id} className="down-arrow" src={downArrow} height="42" width="42"></img>
+                </div>
     })
+
     return (
         <ul id="breweries-ul">
             <h2>{props.city ? props.city : "Community Favorites"}</h2>
             {breweries}
+            
         </ul>
     )
 } 
